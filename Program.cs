aecfb2ae41fe87213a50d92e0d86d39160ab7ab2
ZiganshinIB <elzig3012@gmail.com>
2023-01-28 13:15:43 +0300

    test: Tested project

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@
 
 Console.Write("Введите колличество элементов: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
 string[] texts = new string[n];
-
 for(int i = 0; texts.Length>i; i++){
     Console.Write($"Введите {i+1} элемент текст: ");
     texts[i] = Console.ReadLine();
 }
+
+writeLen3(texts);
