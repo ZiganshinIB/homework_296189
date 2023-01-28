0cae29a6f623df5d8d11d45ee62c3cb567e838a5
ZiganshinIB <elzig3012@gmail.com>
2023-01-28 13:06:13 +0300

    feat: Created Array of string

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите колличество элементов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] texts = new string[n];
+
+for(int i = 0; texts.Length>i; i++){
+    Console.Write($"Введите {i+1} элемент текст: ");
+    texts[i] = Console.ReadLine();
+}
